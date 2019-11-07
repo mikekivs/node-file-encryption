70af93f4b622f2aa6450462183b4c72453fad74a
Michael <mikekivs@gmail.com>
2019-11-07 16:57:00 +0300

    initial files

---
+++ README.md
@@ -0,0 +1,31 @@
+# Node Encryption
+
+An interactive CLI app that can encrypt/decrypt files in a directory using node.
+
+Lib files: [Node Encryption](https://github.com/bbstilson/node-encryption/).
+
+### Usage
+Navigate into the nfe folder
+
+    > cd nfe
+Run nfe
+
+    > node nfe.js
+On the prompt enter the action you want to perform, specify the directory and password.
+
+    Enter [option directory::password] to proceed (option = encrypt|decrypt)
+
+### Encrypting
+    Enter [option directory::password] to proceed (option = encrypt|decrypt)
+    > encrypt ../foo::p@ssW0rd
+      Encrypt success => ../foo/foo.txt
+      Encrypt success => ../foo/bar/bar.txt
+      Encrypt success => ../foo/bar/nay/nay.txt
+
+
+### Decrypting
+    Enter [option directory::password] to proceed (option = encrypt|decrypt)
+    > decrypt ../foo::p@ssW0rd
+      Decryption success => ../foo/foo.txt
+      Decryption success => ../foo/bar/bar.txt
+      Decryption success => ../foo/bar/nay/nay.txt
---
+++ nfe.js
@@ -0,0 +1,76 @@
+const encrypt = require('./lib/encrypt');
+const decrypt = require('./lib/decrypt');
+var readline = require('readline');
+var fs = require('fs');
+var path = require('path');
+const { ALGORITHM, ENCRYPED_EXT, UNENCRYPED_EXT } = require('./lib/constants');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.stdoutMuted = false;
+
+rl.question('Enter [option directory::password] to proceed (option = encrypt|decrypt)\n', function(input) {
+	let inputArr = input.split(' ');
+	let dirArr = inputArr[1].split('::');
+	if(inputArr[0] === 'encrypt' || inputArr[0] === 'decrypt'){
+		processFiles(dirArr[0],inputArr[0],dirArr[1]);
+	}else{
+		console.log('INVALID. Operation must be encrypt or decrypt');
+	}
+	rl.close();
+});
+
+
+rl._writeToOutput = function _writeToOutput(stringToWrite) {
+  if (rl.stdoutMuted)
+    rl.output.write("*");
+  else
+    rl.output.write(stringToWrite);
+};
+
+
+function processFiles(dir,operation,pass){
+	fs.readdir(dir, function (err, files) {
+	  if (err) {
+	    console.error("Could not list the directory.");
+	    process.exit(1);
+	  }
+
+	  files.forEach(function (file, index){
+	  	  fs.stat(dir+'/'+file, function (error, stat) {
+		      if (error) {
+		        console.error("Error stating file.", error);
+		        return;
+		      }
+		      if(stat.isFile()){
+		      	if(operation === "encrypt"){encrypt({ file: dir+'/'+file, password: pass },renameFile);}
+		      	if(operation === "decrypt"){decrypt({ file: dir+'/'+file, password: pass },renameFile);}
+		      }
+		      else if(stat.isDirectory()){
+		      	//console.log("got directory: "+file);
+		      	processFiles(dir+'/'+file,operation,pass);
+		      }
+
+	      });
+	  });
+	});
+}
+
+function renameFile(file,op){
+	var ext = '';
+	if(op == 1){
+		ext = ENCRYPED_EXT;
+	}else if(op == 2){
+		ext = UNENCRYPED_EXT;
+	}
+	fs.unlink(file, () => {
+		fs.rename(file+ext, file, (err) => {
+		  if (err) throw err;
+		});
+	}, (err) => {
+	  if (err) throw err;
+	});
+}
